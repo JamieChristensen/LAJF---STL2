91dae7cf70b57afd4b43ec2fcb0f2ead39e96055
jamie christensen <jamiemoeller@yahoo.dk>
2020-03-14 22:11:40 +0100

    Added additive and asynchronous loading and unloading.
    Fixed naming of scenemanager.cs -> customscenemanager.
    
    Some work on gamemanager.cs (checks for whether or not gamestate-transitions are allowed by checking an array of allowed transitions).
    
    Added eventlisteners to gamemanager.
    
    Created GameManager and SceneManager prefabs.
    
    Added the test-scenes to buildsettings.

---
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/CustomSceneManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+//Responsible for sceneloading, both additive and non-additive.
+//"ALoad" refers to async loading something in. Remember to only load things in async once.
+public class CustomSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private int currentEnvironmentIndex = -1;
+    [SerializeField]
+    private bool isLoadingScene = false;
+
+    public void Start()
+    {
+        GameObject.DontDestroyOnLoad(this);
+    }
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            RequestEnvironmentChange(currentEnvironmentIndex == 1 ? 2 : 1);
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        throw new NotImplementedException();
+    }
+    public void LoadCredits()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RequestEnvironmentChange(int environmentIndex)
+    {
+        //TODO: Check whether or not we can change environment:
+        if (isLoadingScene){
+            return;
+        }
+
+        if(environmentIndex == -1){
+            isLoadingScene = true;
+            StartCoroutine(ALoadEnvironment(environmentIndex));
+            return;
+        }
+
+        isLoadingScene = true;
+        StartCoroutine(ChangeEnvironment(currentEnvironmentIndex, environmentIndex));
+    }
+
+
+    #region AdditiveAsync
+    //ChangeEnvironment combines two other coroutines, to ensure that before loading the next environment, the previous one is always unloaded first.
+    IEnumerator ChangeEnvironment(int currentEnvironment, int newEnvironment)
+    {
+        if (currentEnvironment < 1){
+            StartCoroutine(ALoadEnvironment(newEnvironment));
+            yield break;
+        }
+        AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(currentEnvironment, UnloadSceneOptions.None);
+        asyncUnload.completed += (AsyncOperation) =>
+        {
+            StartCoroutine(ALoadEnvironment(newEnvironment));
+        };
+
+        while (!asyncUnload.isDone)
+        {
+            Debug.Log("Unloading progress: " + asyncUnload.progress);
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+    //Loads in new environments
+    IEnumerator ALoadEnvironment(int sceneIndex)
+    {
+        //Unload current environment (if one is present)
+        //Load in new one
+        //Remove loading-screen (transition)
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
+        asyncLoad.completed += (AsyncOperation) => {
+            currentEnvironmentIndex = sceneIndex;
+            isLoadingScene = false;
+            Debug.Log("Checking how many times this is called");
+        };
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log("Loading progress: " + asyncLoad.progress);
+            yield return null;
+        }
+    }
+
+    IEnumerator AUnloadEnvironment(int sceneIndex)
+    {
+        if (sceneIndex == -1)
+        {
+            yield break;
+        }
+        
+        AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(sceneIndex, UnloadSceneOptions.None);
+
+        while (!asyncUnload.isDone)
+        {
+            Debug.Log("Unloading progress: " + asyncUnload.progress);
+            yield return null;
+        }
+    }
+    #endregion AdditiveAsync
+}
--- Unity project/LAJF-STL UnityProject2D/Assets/Scripts/GameManager.cs
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/GameManager.cs
@@ -3,14 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using STL2.Events;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
     public IntTypeListener playerHPListener;
 
-    public static GameStates gameState = GameStates.none;
-    public enum GameStates{
-        none, /*Before any gamestate is entered, and perhaps for edge-cases/debugging.*/
+    public BoolVariable isGamePaused;
+
+    public VoidTypeListener environmentAndHeroChoiceFinished;  //When the gods finished picking the environment.
+    public VoidTypeListener godsPickedMonsterAndTrait;      //Should be raised when gods finish selecting both monster and traits.
+
+    //KVP of gamestates and their given possible states to transition to - used to prevent unwanted transitions.
+    private KeyValuePair<GameStates, GameStates[]>[] possibleGameStateTransitions = {
+        new KeyValuePair<GameStates, GameStates[]>(GameStates.HeroSelect, new GameStates[] {GameStates.SelectingMonster}),
+
+        new KeyValuePair<GameStates, GameStates[]>(GameStates.SelectingMonster, new GameStates[] {GameStates.SelectingModifier}),
+
+        new KeyValuePair<GameStates, GameStates[]>(GameStates.SelectingModifier, new GameStates[] {GameStates.Encounter}),
+
+        new KeyValuePair<GameStates, GameStates[]>(GameStates.Encounter, new GameStates[] {GameStates.EncounterEnd}),
+
+        new KeyValuePair<GameStates, GameStates[]>(GameStates.EncounterEnd, new GameStates[] {GameStates.InitializingNextScene}),
+
+        new KeyValuePair<GameStates, GameStates[]>(GameStates.InitializingNextScene, new GameStates[] {GameStates.SelectingMonster})
+    };
+
+    public static GameStates gameState = GameStates.None;
+    public enum GameStates
+    {
+        None, /*Before any gamestate is entered, and perhaps for edge-cases/debugging.*/
         HeroSelect, /*Also when environment-change happens.*/
         SelectingMonster,
         SelectingModifier,
@@ -19,43 +41,52 @@ public class GameManager : MonoBehaviour
         InitializingNextScene /*When the next encounter is being loaded and/or a transition is being made between screens (not scenes - we're additively loading stuff)*/
     }
 
+    
 
-    //TODO: 
-    //TODO:
-    //TODO:
+    //These comments are my todo-list for this script - Jamie
+    //TODO: Behaviour when player1 dies.
+    //TODO: Behaviour when player234 finish selection.
 
-
-
-    public void RequestGameStateChange(GameStates requestedState){
-        bool isChangeAllowed = false;
-        //TODO: Take into account what state is changed to and from.
-        switch(requestedState){
-            case GameStates.HeroSelect:
-            isChangeAllowed = true;
-            break; 
-            case GameStates.SelectingMonster:
-            isChangeAllowed = true;
-            break; 
-            case GameStates.SelectingModifier:
-            isChangeAllowed = true;
-            break; 
-            case GameStates.Encounter:
-            isChangeAllowed = true;
-            break; 
-            case GameStates.EncounterEnd:
-            isChangeAllowed = true;
-            break; 
-            case GameStates.InitializingNextScene:
-            isChangeAllowed = true;
-            break; 
-        }
-
-        if(isChangeAllowed) {
-            ChangeGameState(requestedState);
-        }
+    public void Start(){
+        GameObject.DontDestroyOnLoad(this);
     }
 
-    private void ChangeGameState(GameStates gameState){
+    public void RequestGameStateChange(GameStates requestedState)
+    {
+        bool isChangeAllowed = isTransitionAllowed(gameState, requestedState);
+        //Note: If state-changes have to happen in particular ways, a switch-case should be implemented to take these into account.
+        if (!isChangeAllowed)
+        {
+            Debug.Log("Request to change state from: " + gameState + " to " + requestedState + " denied");
+            return;
+        }
+        ChangeGameState(requestedState);
+    }
+
+    private bool isTransitionAllowed(GameStates currentState, GameStates desiredState)
+    {
+        foreach (KeyValuePair<GameStates, GameStates[]> keyValuePair in possibleGameStateTransitions)
+        {
+            if (keyValuePair.Key == currentState)
+            {
+                return keyValuePair.Value.Contains(desiredState);
+            }
+        }
+        return false;
+    }
+
+    private void ChangeGameState(GameStates gameState)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EnvironmentAndHeroChoiceFinished(){
+
+
+        RequestGameStateChange(GameStates.None);
+    }
+
+    public void GodsPickedMonsterAndTrait(){
         throw new NotImplementedException();
     }
 }
