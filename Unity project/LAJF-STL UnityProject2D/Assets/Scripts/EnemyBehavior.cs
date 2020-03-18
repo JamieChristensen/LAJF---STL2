477128e847cadf08f8c036be251de2f66ec51091
Luca Casa <lc301092@gmail.com>
2020-03-18 21:29:20 +0100

    Enemy behavior and scriptableObject (WIP)
    Initial push and file creation..

---
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+
+    public Rigidbody2D rb2;
+    public GameObject target;
+    public Enemy agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.health < 0)
+        {
+            return;
+        }
+
+        MoveTowards(target.transform);
+    }
+
+    private void MoveTowards(Transform tf)
+    {
+        Vector2 direction = tf.position - transform.position;
+        rb2.AddRelativeForce((direction).normalized * agent.speed);
+    }
+
+
+}
---
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/ScriptableObjectScripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Enemy", menuName = "ScriptableObject/Enemy")]
+public class Enemy : ScriptableObject
+{
+    public new string name;
+
+    public Sprite sprite;
+
+    public int health;
+    public int damage;
+    public int speed;
+
+    //public Trait trait;
+    // OG stats
+    // public int length;
+    // public int width;
+
+}
