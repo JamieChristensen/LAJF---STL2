f878a444e4034a910c6b49844e8702dd617bfcae
Luca Casa <lc301092@gmail.com>
2020-03-19 21:20:01 +0100

    Enemy behavior for testing
    melee attack not yet done.

--- Unity project/LAJF-STL UnityProject2D/Assets/Scripts/EnemyBehavior.cs
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/EnemyBehavior.cs
@@ -9,29 +9,77 @@ public class EnemyBehavior : MonoBehaviour
     public Rigidbody2D rb2;
     public GameObject target;
     public Enemy agent;
+    public GameObject bulletObj;
+
+    private float currentHealth;
+    private int cooldownTimer;
+    private const float PROJECTILE_SPEED = 5;
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("P1");
+        currentHealth = agent.health;
+        cooldownTimer = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if(agent.health < 0)
+        // Am I alive?
+        if (currentHealth < 0)
         {
             return;
         }
+       float distance = (target.transform.position - gameObject.transform.position).magnitude;
+        // start of behavior tree here
 
+        if (agent.range >= distance)
+        {
+            if (cooldownTimer <= 0)
+            {
+                Debug.Log("attacking");
+                cooldownTimer = agent.attackSpeed;
+                Attack(agent.attackType);
+                return;
+            }
+        }
         MoveTowards(target.transform);
+        cooldownTimer--;
+    }
+
+    private void Attack(string attackType)
+    {
+        if (attackType == "melee")
+            MeleeAttack();
+        if (attackType == "range")
+            RangedAttack();
     }
 
     private void MoveTowards(Transform tf)
     {
         Vector2 direction = tf.position - transform.position;
-        rb2.AddRelativeForce((direction).normalized * agent.speed);
+        rb2.velocity = ((direction).normalized * agent.speed);
     }
 
+    void MeleeAttack()
+    {
+        // Vector2 direction = (target.transform.position - transform.position);
+        // Collider2D attack = RaycastHit2D();
+        throw new NotImplementedException("melee attack");
+    }
 
+    void RangedAttack()
+    {
+       Vector2 direction = (target.transform.position - transform.position);
+       GameObject bullet = Instantiate(bulletObj, transform.position + (((Vector3)direction) * 0.2f), Quaternion.identity);
+       bullet.GetComponent<Rigidbody2D>().velocity = direction * PROJECTILE_SPEED;
+       // bullet.damage = agent.damage; 
+    }
+
+    public void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
 }
--- Unity project/LAJF-STL UnityProject2D/Assets/Scripts/ScriptableObjectScripts/Enemy.cs
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/ScriptableObjectScripts/Enemy.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="New Enemy", menuName = "ScriptableObject/Enemy")]
 public class Enemy : ScriptableObject
 {
+    // generic
     public new string name;
-
-    public Sprite sprite;
-
     public int health;
+    public float speed;
+
+    // visual
+    public Sprite sprite;
+    public float scaleFactor; 
+
+    // combat 
     public int damage;
-    public int speed;
+    public string attackType;
+    public int range;
+    public int attackSpeed;
+    
+    // can be called traits that is made
+    public object[] traits; 
+
+
 
     //public Trait trait;
     // OG stats
