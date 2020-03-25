93d4b85ea1ed5377db33cac7aa665baa9f28bbc9
Luca Casa <lc301092@gmail.com>
2020-03-25 23:17:28 +0100

    narrator behavior WIP
    NarratorBehavior.cs
    - added basic functions
    
    TTS.cs
    - added intial work on text querys with Watson API

---
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/NarratorBehaviour.cs
@@ -0,0 +1,84 @@
+﻿using IBM.Watson.DeveloperCloud.Services.TextToSpeech.v1;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class NarratorBehaviour : MonoBehaviour
+    {
+    public AudioSource audio;
+    public Camera mainCam;
+    public int getInSpeed = 5;
+    public Rigidbody2D rb2;
+
+    public Vector2 previousPosition;
+    
+
+
+        void Start()
+        {
+        }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+        {
+
+            if(Input.GetMouseButtonUp(0)){
+
+                EnterScene();
+                
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                exitScene();
+            }
+
+            if (Input.GetMouseButtonUp(2))
+            {
+            Debug.Log("PRESSEd");
+                RandomizePosition();
+            }
+            
+        }
+
+    private void exitScene()
+    {
+        Vector2 direction = - (mainCam.transform.position - transform.position).normalized;
+        rb2.velocity = direction * getInSpeed;
+    }
+
+    void EnterScene()
+    {
+        //RandomizePosition();
+        Vector2 direction = (mainCam.transform.position - transform.position).normalized;
+        rb2.velocity = direction*getInSpeed;
+        audio.Play();
+    }
+
+    void RandomizePosition()
+    {
+        float y, x;
+        //determine which side to use / width or height
+        if (UnityEngine.Random.Range(0,1) >= 0.5f)
+        {
+            y = UnityEngine.Random.Range(0, mainCam.scaledPixelHeight);
+            x = 0;
+        }
+        else{
+            y = 0;
+            x = UnityEngine.Random.Range(0, mainCam.scaledPixelWidth);
+        }
+        Vector3 newPos = new Vector3(x, y);
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * 100);
+    }
+
+           
+       
+
+    }
+
