3dae81c1d2bef9ab428ee32640dbfc77348eb54e
Mightydeeze <frederik.d.c96@gmail.com>
2020-03-26 20:38:07 +0100

    Changed Choosing scenes and created HealthBar Prefab

---
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class HealthBar : MonoBehaviour
+{
+    public Canvas healthbarCanvas;
+    public TMP_Text hitPointsText;
+    public Transform HealthBarScaleTransform;
+    public int maxHp;
+
+
+    public void VisualiseHealthChange(int currentHealth)
+    {
+
+        hitPointsText.SetText(currentHealth + " HP");
+        HealthBarScaleTransform.localScale.x = (maxHp / 100) * currentHealth;
+
+    }
+
+
+
+}
