1934dd4497f1530c64458fc73cdf2cf0dd745c62
Mightydeeze <frederik.d.c96@gmail.com>
2020-03-27 00:20:04 +0100

    HealthBar is Mobaready and crisp

--- Unity project/LAJF-STL UnityProject2D/Assets/Scripts/UI/HealthBar.cs
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/UI/HealthBar.cs
@@ -9,18 +9,50 @@ public class HealthBar : MonoBehaviour
 {
     public Canvas healthbarCanvas;
     public TMP_Text hitPointsText;
-    public Transform HealthBarScaleTransform;
+    public Transform CurrentHealthFillTransform;
+    public Transform DamageHealthFillTransform;
+
+    bool freezeVisibleDamage = false;
+    float freezeTime = 0.5f, timer = 0;
     public int maxHp;
+
+    float damageScaleX = 5, currentHealthScaleX = 5;
 
 
     public void VisualiseHealthChange(int currentHealth)
     {
-
+        freezeVisibleDamage = true;
+        timer = 0;
         hitPointsText.SetText(currentHealth + " HP");
-        HealthBarScaleTransform.localScale = new Vector3(((maxHp / 100) * currentHealth)/20, 1,1);
+        currentHealthScaleX = ((maxHp / 100) * currentHealth) / 20;
+        CurrentHealthFillTransform.localScale = new Vector3(currentHealthScaleX, 1,1);
+        
+
+
+
+
 
         Debug.Log(currentHealth);
        // Debug.Log(HealthBarScaleTransform.localScale);
+
+    }
+
+    private void Update()
+    {
+        if(freezeVisibleDamage == true)
+        {
+            timer += Time.deltaTime;
+            if(timer > freezeTime)
+            {
+                freezeVisibleDamage = false;
+            }
+        }
+        else
+        {
+            damageScaleX = Mathf.Lerp(damageScaleX, currentHealthScaleX, Time.deltaTime * 4f);
+            DamageHealthFillTransform.localScale = new Vector3(damageScaleX, 1, 1);
+        }
+        
 
     }
 
