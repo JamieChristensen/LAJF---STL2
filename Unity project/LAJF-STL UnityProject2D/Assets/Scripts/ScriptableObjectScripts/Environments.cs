f9df532cc1f6f97cdfed4544359a71b66c67a4b5
Mightydeeze <frederik.d.c96@gmail.com>
2020-04-07 16:08:07 +0200

    Created Environments.cs (scriptable)

---
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/ScriptableObjectScripts/Environments.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Environments", menuName = "ScriptableObject/Environments")]
+public class Environments : ScriptableObject
+{
+
+    public string environmentName;
+    public string description;
+    public Sprite environmentSprite;
+
+    public int environmentIndices;
+
+}
+
