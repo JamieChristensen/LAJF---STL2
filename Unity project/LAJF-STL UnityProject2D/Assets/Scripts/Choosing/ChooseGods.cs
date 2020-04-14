2fcb72e38d283e14ac36ffe87c2268b37cb88fc0
Mightydeeze <frederik.d.c96@gmail.com>
2020-04-14 23:12:00 +0200

    Customizing choice scenes

--- Unity project/LAJF-STL UnityProject2D/Assets/Scripts/Choosing/ChooseGods.cs
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/Choosing/ChooseGods.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using STL2.Events;
+using UnityEngine.UI;
 
 
 public class ChooseGods : MonoBehaviour
@@ -21,6 +22,9 @@ public class ChooseGods : MonoBehaviour
 
     [SerializeField]
     private int[] choices = new int[3]; //player2 choice in 0, player3 choice in 1, player4 choice in 2. Range 1-5. 0 if nothing has been chosen.
+    public Image[] backgrounds;
+    public Image[] highlights;
+    public Image[] overlays;
     private int p2Index, p3Index, p4Index;
     private bool p2LockedIn, p3LockedIn, p4LockedIn;
 
@@ -62,7 +66,7 @@ public class ChooseGods : MonoBehaviour
 
     public void Update()
     {
-        if (p2LockedIn && p3LockedIn && p4LockedIn && lockedIn == false)
+        if (p2LockedIn /*&& p3LockedIn && p4LockedIn */ && lockedIn == false)
         {
             for (int i = 0; i < choices.Length; i++)
             {
@@ -156,6 +160,23 @@ public class ChooseGods : MonoBehaviour
             return;
         }
         choices[godNumber] = newSelection;
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            if (i == newSelection)
+            {
+                backgrounds[i].gameObject.SetActive(true);
+                highlights[i].gameObject.SetActive(true);
+                overlays[i].gameObject.SetActive(false);
+            }
+            else
+            {
+                backgrounds[i].gameObject.SetActive(false);
+                highlights[i].gameObject.SetActive(false);
+                overlays[i].gameObject.SetActive(true);
+            }
+            
+        }
+        
         //Reset text on each element:
         foreach (TextMeshProUGUI choiceText in choiceTMProText)
         {
