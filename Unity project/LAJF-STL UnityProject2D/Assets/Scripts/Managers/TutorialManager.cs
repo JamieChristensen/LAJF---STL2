6c0922fbf87cebf9c4092e69a39d3ab8b059bf8a
ubiquitousman <35701742+ubiquitousman@users.noreply.github.com>
2020-05-20 18:15:16 +0200

    Task continue: '3) Tutorial / introduction'

--- Unity project/LAJF-STL UnityProject2D/Assets/Scripts/Managers/TutorialManager.cs
+++ Unity project/LAJF-STL UnityProject2D/Assets/Scripts/Managers/TutorialManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] tutorialPanels;
+    int panelIndex;
+
+    public void SwitchPanel(int addition)
     {
-        
+        panelIndex += addition;
+
+        if (panelIndex == tutorialPanels.Length)
+            panelIndex = 0;
+        else if (panelIndex < 0)
+            panelIndex = tutorialPanels.Length - 1;
+
+        for (int i = 0; i < tutorialPanels.Length; i++)
+        {
+            if (i == panelIndex)
+            {
+                tutorialPanels[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                tutorialPanels[i].gameObject.SetActive(false);
+            }
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
